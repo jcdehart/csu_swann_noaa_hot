c0d5559b411c1fa16061b8c8134954212e8d72ce
Jen DeHart <jcdehart@colostate.edu>
2026-02-26 18:12:08 -0700

    Air Force only flights

--- trigger_swann_from_vdm.sh
+++ trigger_swann_from_vdm.sh
@@ -4,6 +4,6 @@ outputString=$(python hot_run_from_vdm.py)
 
 set $outputString
 
-echo "python hot_main_run_hdobsonly.py $1 $2 $3 N"
+echo "python hot_main_run_hdobsonly.py $1 $2 $3 A"
 
-python hot_main_run_hdobsonly.py $1 $2 $3 N
+python hot_main_run_hdobsonly.py $1 $2 $3 A
